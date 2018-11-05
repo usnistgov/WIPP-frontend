be0cbeb0daaf9d75acac07ea7574950ab4e78913
Mylene Simon <mylene.simon@nist.gov>
2018-11-05 10:12:23 -0500

    Feature/issue3 integrate workflows component

---
+++ src/app/workflow/workflow.service.ts
@@ -0,0 +1,77 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+import {HttpParams} from '../../../node_modules/@angular/common/http';
+import {map} from 'rxjs/operators';
+import {PaginatedWorkflows, Workflow} from './workflow';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  params: {}
+};
+
+@Injectable({providedIn: 'root'})
+export class WorkflowService {
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  private workflowsUrl = environment.apiRootUrl + '/workflows';
+  private jobsUrl = environment.apiRootUrl + '/jobs';
+
+  getWorkflows(params): Observable<PaginatedWorkflows> {
+    if (params) {
+      const page = params.pageIndex ? params.pageIndex : null;
+      const size = params.size ? params.size : null;
+      const httpParams = new HttpParams().set('page', page).set('size', size);
+      httpOptions.params = httpParams;
+    }
+    return this.http.get<any>(this.workflowsUrl, httpOptions).pipe(
+      map((result: any) => {
+        result.workflows = result._embedded.workflows;
+        return result;
+      }));
+  }
+
+  getWorkflow(id: string): Observable<Workflow> {
+    return this.http.get<Workflow>(`${this.workflowsUrl}/${id}`);
+  }
+
+  // getSchemaForm(pluginName: string): Observable<Object> {
+  //   httpOptions.params = null;
+  //   return this.http.post<Object>(
+  //     this.workflowsUrl + '/search-ngx',
+  //     {'name': pluginName},
+  //     httpOptions
+  //   );
+  // }
+
+  // getCollections(): string[] {
+  //   return this.collection.concat(this.outputCollection);
+  // }
+  //
+  // addCollection(collectionName): void {
+  //   this.outputCollection.push(collectionName);
+  // }
+  //
+  // resetCollection(): void {
+  //   this.outputCollection = [];
+  // }
+
+  createWorkflow(workflow: Workflow): Observable<Workflow> {
+    return this.http.post<Workflow>(this.workflowsUrl, workflow);
+  }
+
+  createJob(job): Observable<Object> {
+    return this.http.post<Object>(this.jobsUrl, job);
+  }
+
+  submitWorkflow(workflow): Observable<Workflow> {
+    return this.http.post<Workflow>(
+      `${this.workflowsUrl}/${workflow.id}/submit`,
+      null,
+      httpOptions
+    );
+  }
+}
