91f533245ed0d3c5b07202a9a3202798929a1e20
Samiasa <samia.benjida@telecomnancy.net>
2019-03-29 10:47:38 -0400

    feat: stitching vectors list page

--- src/app/stitching-vector/stitching-vector-list/stitching-vector-list.component.ts
+++ src/app/stitching-vector/stitching-vector-list/stitching-vector-list.component.ts
@@ -4,6 +4,8 @@ import {StitchingVector} from '../stitching-vector';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {StitchingVectorNewComponent} from '../stitching-vector-new/stitching-vector-new.component';
 import {StitchingVectorService} from '../stitching-vector.service';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {merge, of as observableOf} from 'rxjs';
 
 @Component({
   selector: 'app-stitching-vector-list',
@@ -14,14 +16,14 @@ import {StitchingVectorService} from '../stitching-vector.service';
   imports: [MatTableModule, MatTableDataSource, MatTable]
 })
 export class StitchingVectorListComponent implements OnInit {
-  displayedColumns: string[] = ['Name', 'Creation date', 'Number of time slices'];
-  imagesCollections: StitchingVector[] = [];
+  displayedColumns: string[] = ['name', 'creationDate', 'numberOfTimeSlices'];
+  stitchingVectors: StitchingVector[] = [];
 
   resultsLength = 0;
   pageSize = 20;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  // @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private stitchingVectorService: StitchingVectorService,
@@ -31,12 +33,35 @@ export class StitchingVectorListComponent implements OnInit {
 
 
   ngOnInit() {
+      this.getStitchingVectors();
+  }
+
+   getStitchingVectors(): void {
+    this.paginator.page.pipe(
+      startWith({}),
+      switchMap(() => {
+        const params = {
+          pageIndex: this.paginator.pageIndex,
+          size: this.pageSize
+        };
+        return this.stitchingVectorService.getStitchingVectors(params);
+      }),
+      map(data => {
+        this.resultsLength = data.page.totalElements;
+        return data.stitchingVectors;
+      }),
+       catchError(() => {
+        return observableOf([]);
+      })
+    ).subscribe(data => this.stitchingVectors = data);
   }
 
 
   createNew() {
     const modalRef = this.modalService.open(StitchingVectorNewComponent, {size: 'lg'});
     modalRef.componentInstance.modalReference = modalRef;
+    modalRef.result.then((result) =>
+      this.getStitchingVectors() );
   }
 
 }
