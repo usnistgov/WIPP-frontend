773f3e69cbc7badb750f8c6d857db4714dac2828
Samiasa <samia.benjida@telecomnancy.net>
2019-03-29 10:47:38 -0400

    feat: stitching vectors list page

--- src/app/stitching-vector/stitching-vector.service.ts
+++ src/app/stitching-vector/stitching-vector.service.ts
@@ -1,7 +1,10 @@
 import {Injectable} from '@angular/core';
-import {StitchingVector} from './stitching-vector';
+import {PaginatedStitchingVector, StitchingVector} from './stitching-vector';
 import {environment} from '../../environments/environment';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {PaginatedImagesCollections} from '../images-collection/images-collection';
 
 @Injectable({
   providedIn: 'root'
@@ -20,10 +23,36 @@ export class StitchingVectorService {
     formData.append('name', stitchingVector.name);
     stitchingVector.note ? formData.append('message', stitchingVector.note) : formData.append('message', '');
 
-    this.http.post<StitchingVector>(this.stitchingVectorsUrl + '/upload', formData);
+    this.http.post<StitchingVector>(this.stitchingVectorsUrl + '/upload', formData)
+      .subscribe(res => {
+        console.log(res);
+      });
   }
 
-  getVectorsUrl(stitchingVector: StitchingVector): string {
+  getStitchingVectorsUrl(stitchingVector: StitchingVector): string {
     return `${this.stitchingVectorsUrl}/${stitchingVector.id}/vector`; // TODO: check path
+  }
+
+  getStitchingVector() {
+    return this.http.get(this.stitchingVectorsUrl);
+  }
+
+  getStitchingVectors(params): Observable<PaginatedStitchingVector> {
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'}),
+      params: {}
+    };
+    if (params) {
+      const page = params.pageIndex ? params.pageIndex : null;
+      const size = params.size ? params.size : null;
+      const httpParams = new HttpParams().set('page', page).set('size', size);
+      httpOptions.params = httpParams;
+    }
+    return this.http.get<any>(this.stitchingVectorsUrl, httpOptions).pipe(
+      map((result: any) => {
+        console.log(result);
+        result.stitchingVectors = result._embedded.stitchingVectors;
+        return result;
+      }));
   }
 }
