ba8e04c010d6957bd08882a98bcd2b24719cade9
mno <mohamed.ouladi@nist.gov>
2019-05-17 13:27:56 -0400

    - Re-implemented the pagination for the tables images and metadata files in the image collection detail page - implemented sorting for the tables above - Added page size options for both tables (items per page)

--- src/app/images-collection/images-collection.service.ts
+++ src/app/images-collection/images-collection.service.ts
@@ -78,7 +78,8 @@ export class ImagesCollectionService {
     if (params) {
       const page = params.pageIndex ? params.pageIndex : null;
       const size = params.size ? params.size : null;
-      const httpParams = new HttpParams().set('page', page).set('size', size);
+      const sort = params.sort ? params.sort : null;
+      const httpParams = new HttpParams().set('page', page).set('size', size).set('sort', sort);
       httpOptions.params = httpParams;
     }
     console.log(httpOptions);
@@ -98,7 +99,8 @@ export class ImagesCollectionService {
     if (params) {
       const page = params.pageIndex ? params.pageIndex : null;
       const size = params.size ? params.size : null;
-      const httpParams = new HttpParams().set('page', page).set('size', size);
+      const sort = params.sort ? params.sort : null;
+      const httpParams = new HttpParams().set('page', page).set('size', size).set('sort', sort);
       httpOptions.params = httpParams;
     }
     console.log(httpOptions);
