3f37fd38b6e47a8719e05aa5ffc4061c66340041
mno <mohamed.ouladi@nist.gov>
2019-05-22 12:13:10 -0400

    feat: implemented filtering by name and by number of images in the Image Collection list page

--- src/app/images-collection/images-collection-list/images-collection-list.component.ts
+++ src/app/images-collection/images-collection-list/images-collection-list.component.ts
@@ -25,6 +25,8 @@ export class ImagesCollectionListComponent implements OnInit {
   isLoadingResults = true;
   pageSizeOptions: number[] = [10, 25, 50, 100];
   paramsChange: BehaviorSubject<{index: number, size: number, sort: string}>;
+  filterNameChange: BehaviorSubject<string>;
+  filterNbOfImagesChange: BehaviorSubject<string>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -38,6 +40,8 @@ export class ImagesCollectionListComponent implements OnInit {
       size: this.pageSize,
       sort: 'creationDate,desc'
     });
+    this.filterNameChange = new BehaviorSubject('');
+    this.filterNbOfImagesChange = new BehaviorSubject('');
   }
 
   sortChanged(sort) {
@@ -49,16 +53,54 @@ export class ImagesCollectionListComponent implements OnInit {
     this.paramsChange.next({index: page.pageIndex, size: page.pageSize, sort: this.paramsChange.value.sort});
   }
 
+  applyFilterByName(filterValue: string) {
+    this.filterNameChange.next(filterValue);
+  }
+
+  applyFilterByNbOfImages(filterValue: string) {
+    this.filterNbOfImagesChange.next(filterValue);
+  }
+
   ngOnInit() {
     const paramsObservable = this.paramsChange.asObservable();
-    this.imagesCollections = paramsObservable.pipe(
-      switchMap((page) => {
+    const filterNameObservable = this.filterNameChange.asObservable();
+    const filterNbOfImagesObservable = this.filterNbOfImagesChange.asObservable();
+    this.imagesCollections = combineLatest(paramsObservable, filterNameObservable, filterNbOfImagesObservable).pipe(
+      switchMap(([page, filterByName, filterByNbOfImages]) => {
         this.isLoadingResults = true;
         const params = {
           pageIndex: page.index,
           size: page.size,
           sort: page.sort
         };
+        if (filterByName) {
+          return this.imagesCollectionService.getImagesCollectionsByNameContainingIgnoreCase(params, filterByName).pipe(
+            map((data) => {
+              this.isLoadingResults = false;
+              this.resultsLength = data.page.totalElements;
+              return data.imagesCollections;
+            }),
+            catchError(() => {
+              this.isLoadingResults = false;
+              return observableOf([]);
+            })
+          );
+        }
+        if (filterByNbOfImages) {
+          return this.imagesCollectionService
+            .getImagesCollectionsByNameContainingIgnoreCaseAndNumberOfImages(params, '', filterByNbOfImages)
+            .pipe(
+              map((data) => {
+                this.isLoadingResults = false;
+                this.resultsLength = data.page.totalElements;
+                return data.imagesCollections;
+              }),
+              catchError(() => {
+                this.isLoadingResults = false;
+                return observableOf([]);
+              })
+            );
+        }
         return this.imagesCollectionService.getImagesCollections(params).pipe(
           map((data) => {
             this.isLoadingResults = false;
@@ -66,20 +108,21 @@ export class ImagesCollectionListComponent implements OnInit {
             return data.imagesCollections;
           }),
           catchError(() => {
-              this.isLoadingResults = false;
-              return observableOf([]);
-            })
+            this.isLoadingResults = false;
+            return observableOf([]);
+          })
         );
       })
     );
   }
+
   createNew() {
-    const modalRef = this.modalService.open(ImagesCollectionNewComponent, { size: 'lg' });
+    const modalRef = this.modalService.open(ImagesCollectionNewComponent, {size: 'lg'});
     modalRef.componentInstance.modalReference = modalRef;
     modalRef.result.then((result) => {
       this.imagesCollectionService.createImagesCollection(result).subscribe(imagesCollection => {
         const imageCollId = imagesCollection ? imagesCollection.id : null;
-        this.router.navigate(['images-collection', imageCollId ]);
+        this.router.navigate(['images-collection', imageCollId]);
       });
     }, (reason) => {
       console.log('dismissed');
