50c432728dff016578c30c5479fe7a9d37e5d534
Mylene Simon <mylene.simon@nist.gov>
2019-06-05 10:15:26 -0400

    feat: integrate wdzt as directive and add to pyramid detail view

---
+++ src/app/wdzt/wdzt.directive.spec.ts
@@ -0,0 +1,8 @@
+import { WdztDirective } from './wdzt.directive';
+
+describe('WdztDirective', () => {
+  it('should create an instance', () => {
+    const directive = new WdztDirective();
+    expect(directive).toBeTruthy();
+  });
+});
