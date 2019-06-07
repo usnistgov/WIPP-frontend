6015ead099b27130edcbd19bcc0c4cd4061df87c
mno <mohamed.ouladi@nist.gov>
2019-06-07 10:47:37 -0400

    feat: implemented pagination + sorting by name and by creation date + filtering by name in the plugins list page

--- src/app/plugin/plugin.module.ts
+++ src/app/plugin/plugin.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {MatCheckboxModule, MatPaginatorModule, MatSortModule, MatTableModule} from '@angular/material';
+import {MatCheckboxModule, MatInputModule, MatPaginatorModule, MatSortModule, MatTableModule} from '@angular/material';
 import {FormsModule} from '@angular/forms';
 import {PluginRoutingModule} from './plugin-routing.module';
 import {PluginListComponent} from './plugin-list/plugin-list.component';
@@ -15,6 +15,7 @@ import {PluginDetailComponent} from './plugin-detail/plugin-detail.component';
     MatPaginatorModule,
     MatTableModule,
     MatSortModule,
+    MatInputModule,
     FormsModule,
     MatCheckboxModule
   ],
