6ca7a19b8e2880d73b5ac955c9dc858e3e5d4e9f
Samiasa <samia.benjida@nist.gov>
2019-09-13 13:40:29 -0400

    fix: remove unused imports

--- src/app/dynamic-content/dynamic-content.module.ts
+++ src/app/dynamic-content/dynamic-content.module.ts
@@ -1,13 +1,9 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {DynamicContentComponent} from './dynamic-content.component';
 import {CommonModule} from '@angular/common';
 import {UnknownDynamicComponent} from './unknown-dynamic.component';
-import {
-  StitchingVectorTemplateComponent
-} from '../stitching-vector/stitching-vector-template/stitching-vector-template.component';
-import {
-  ImagesCollectionTemplateComponent
-} from '../images-collection/images-collection-template/images-collection-template.component';
+import {StitchingVectorTemplateComponent} from '../stitching-vector/stitching-vector-template/stitching-vector-template.component';
+import {ImagesCollectionTemplateComponent} from '../images-collection/images-collection-template/images-collection-template.component';
 import {PyramidTemplateComponent} from '../pyramid/pyramid-template/pyramid-template.component';
 import {TensorflowModelTemplateComponent} from '../tensorflow-model/tensorflow-model-template/tensorflow-model-template.component';
 import {TensorboardLogsTemplateComponent} from '../tensorflow-model/tensorflow-model-template/tensorboard-logs-template.component';
