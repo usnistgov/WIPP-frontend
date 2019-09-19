25b84f49937026d4ab14b466e59e499091e6773d
Mylene Simon <mylene.simon@nist.gov>
2019-09-19 09:37:05 -0400

    fix: get all plugins sorted by names when creating a job

--- src/app/workflow/workflow-detail/workflow-detail.component.ts
+++ src/app/workflow/workflow-detail/workflow-detail.component.ts
@@ -48,7 +48,7 @@ export class WorkflowDetailComponent implements OnInit {
   ngOnInit() {
     this.workflowService.getWorkflow(this.workflowId).subscribe(workflow =>
       this.workflow = workflow);
-    this.pluginService.getPlugins(null)
+    this.pluginService.getPlugins({ size: Number.MAX_SAFE_INTEGER, sort: 'name' })
       .subscribe(plugins => {
         this.pluginList = plugins.plugins;
         this.generateSchema(this.pluginList);
