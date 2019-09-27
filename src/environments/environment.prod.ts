21d3b4d50a7ad6d11754670f7eb409c7ab511b6b
MyleneSimon <mylene.hm.simon@gmail.com>
2019-09-27 11:55:25 -0400

    Revert "feat: add nginx locations for polus tools"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   version: version,
   apiRootUrl: '/api',
   tensorboardUrl: '/tensorboard',
-  jupyterNotebooksUrl: '/jupyterLab',
-  plotsUiUrl: '/visionUi'
+  jupyterNotebooksUrl: 'http://j.ci.aws.labshare.org',
+  plotsUiUrl: 'http://vision-ui.ci.aws.labshare.org'
 };
