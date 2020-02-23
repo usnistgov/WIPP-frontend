a51c80c503a5ea1779159bb7f2084891c8e05f5b
Olivier Gouellain <ogouellain@gmail.com>
2020-02-23 11:15:42 +0100

    code cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,30 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { environment} from '../environments/environment';
 import {AppConfigService} from './app-config.service';
 import {KeycloakService} from './services/keycloak/keycloak.service';
-import { KeycloakInstance } from 'keycloak-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'WIPP';
   version = environment.version;
   isNavbarCollapsed = true;
   jupyterNotebooksLink = '';
   plotsUiLink = '';
-  public keycloakAuth: KeycloakInstance;
 
   constructor(private appConfigService: AppConfigService, private keycloak: KeycloakService) {
     this.jupyterNotebooksLink = this.appConfigService.getConfig().jupyterNotebooksUrl;
     this.plotsUiLink = this.appConfigService.getConfig().plotsUiUrl;
   }
 
-  ngOnInit() {
-    this.keycloakAuth = this.keycloak.getKeycloakAuth();
+  isLoggedIn() {
+    return this.keycloak.isLoggedIn();
   }
-
   login() {
     this.keycloak.login();
   }
