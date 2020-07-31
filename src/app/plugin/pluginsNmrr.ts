f501862d1a735b6831561d83b63b48a10ad2f415
Samiasa <samia.benjida@nist.gov>
2020-07-31 18:36:28 -0400

    feat: get plugins from Nmrr and register plugins

--- src/app/plugin/pluginsNmrr.ts
+++ src/app/plugin/pluginsNmrr.ts
@@ -1,4 +1,4 @@
-//it will store the page retrieved from NMRR
+// it will store the page retrieved from NMRR
 export interface PluginsNmrr {
   count: number;
   next: string;
@@ -7,54 +7,15 @@ export interface PluginsNmrr {
   _links: any;
 }
 
-/*
-export class FullSoftware {
-  id: any;
-  template: Template;
-  user_id:String;
-  title:String;
-  xml_content:string;
-  last_modification_date:String;
-  _links: any;
-}
-
-export class Template{
-   id:String;
-   filename:String;
-   content:String;
-   hash:String;
-   _display_name:String;
-   dependencies:String[];
-   _cls:String;
-   _links: any;
-}*/
-
-//it will store each item in the plugin list
+// it will store each item in the plugin list
 export class PluginNmrr {
   id: string;
   template: string;
   workspace: string;
-  user_id:string;
-  title:string;
-  xml_content:string;
-  last_modification_date:string;
-  _links: any;
-
-}
-
-
-// xml data
-export class PluginApi {
-  id: string;
-  name: string;
-  description: string;
-  version: string;
+  user_id: string;
   title: string;
-  author: string;
-  institution: string;
-  repository: string;
-  website: string;
-  citation: string;
-  containerId: string;
-  jsondata :string;
+  xml_content: string;
+  last_modification_date: string;
+  manifest: string;
+  _links: any;
 }
