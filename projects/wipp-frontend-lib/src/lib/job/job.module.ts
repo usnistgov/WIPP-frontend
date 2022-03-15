ae421858095ef861db3f8b93f975a2c4fe877cd9
mohamedOuladi <mohamed.ouladi@labshare.org>
2022-03-15 13:42:38 -0400

    fix: removed unused component

--- projects/wipp-frontend-lib/src/lib/job/job.module.ts
+++ projects/wipp-frontend-lib/src/lib/job/job.module.ts
@@ -3,14 +3,11 @@ import {DynamicContentComponent} from '../dynamic-content/dynamic-content.compon
 import {CommonModule} from '@angular/common';
 import {DynamicContentModule} from '../dynamic-content/dynamic-content.module';
 import { HttpClientModule } from '@angular/common/http';
-import { JobFormComponent } from './job-form/job-form.component';
-
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
-  ],
-  declarations: [JobFormComponent]
+  ]
 })
 export class JobModule { }
